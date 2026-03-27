6db941db21cecc854215ef138f94d9fc7e7cc03b
saray-cod <puertolinni@gmail.com>
2026-03-27 00:28:24 -0500

    feat(apropiacion): punto apropiacion 3 realizado closes #8

--- Apropiacion/punto_3.js
+++ Apropiacion/punto_3.js
@@ -1,6 +1,9 @@
-for (let i=1; i<=10; i++) {
+function mostrarNumerosMayoresA50() {
+  for (let i=1; i<=10; i++) {
     let numero = parseInt(prompt("Ingrese un número: "));
     if (numero > 50) {
         console.log("numeros mayores a 50: " + numero);
     }
-}  
+  } 
+}
+mostrarNumerosMayoresA50();
