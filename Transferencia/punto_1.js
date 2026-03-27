bfd1ebdf0789c84c7278bd569e876f78951ec006
saray-cod <puertolinni@gmail.com>
2026-03-27 00:36:58 -0500

    feat(transferencia): punto transferencia 1 realizado closes #14

---
+++ Transferencia/punto_1.js
@@ -0,0 +1,15 @@
+function mostrarNumerosPrimos() {
+  for (let i = 2; i <= 50; i++) {
+    let esPrimo = true;
+    for (let j = 2; j < i; j++) {
+      if (i % j === 0) {
+        esPrimo = false;
+        break;
+      }
+    }
+    if (esPrimo) {
+      console.log(i);
+    }
+  }
+}
+mostrarNumerosPrimos();
